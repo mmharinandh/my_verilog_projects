f2780e7cd62c1d1b7f7b1f37d5794f19d459fe5e
mmharinandh <154574686+mmharinandh@users.noreply.github.com>
2024-08-11 19:52:06 +0530

    Update testbench.v

--- carry_lookahead_adder_using_datapath_and_controller/testbench.v
+++ carry_lookahead_adder_using_datapath_and_controller/testbench.v
@@ -29,27 +29,13 @@ reg [15:0] data_in;
 wire done;
 wire [16:0] data_out;
 
-carry_lookahead_adder_datapath dut1 (.clk(clk), 
-                                     .load_a(load_a), 
-                                     .load_b(load_b), 
-                                     .clr_a(clear_a),
-                                     .clr_b(clear_b),
+carry_lookahead_adder #(.N(N)) dut (.clk(clk),
+                                     .start(start),
                                      .carry_in(cin),
                                      .data_in(data_in),
+                                     .done(done),
                                      .data_out(data_out));
-                                     
-carry_lookahead_adder_control dut2 (.clk(clk),
-                                    .load_a(load_a),
-                                    .load_b(load_b),
-                                    .clear_a(clear_a),
-                                    .clear_b(clear_b),
-                                    .start(start),
-                                    .done(done));
                                     
-initial
-begin 
-    $monitor("%h", dut1.A_out);
-end
                                     
 initial
 begin
