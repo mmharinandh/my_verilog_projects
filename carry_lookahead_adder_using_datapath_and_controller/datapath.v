059eafbf0cc967e7d3990e6fd5742d7030feeca4
mmharinandh <154574686+mmharinandh@users.noreply.github.com>
2024-08-11 19:53:33 +0530

    Update datapath.v

--- carry_lookahead_adder_using_datapath_and_controller/datapath.v
+++ carry_lookahead_adder_using_datapath_and_controller/datapath.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Harinandh M M
 // 
 // Create Date: 10.08.2024 07:32:41
 // Design Name: 
